c6de83544c6c84437e657bb938389c4ae4c9c8c8
03-JS <pyjoso3vitaslim@gmail.com>
2023-08-10 17:16:39 +0200

    Created CurseOfAttrition and Overshield

---
+++ src/main/java/net/fabricmc/extraenchantments/curses/CurseOfAttrition.java
@@ -0,0 +1,48 @@
+package net.fabricmc.extraenchantments.curses;
+
+import net.fabricmc.extraenchantments.enchantments.Swiftness;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
+
+public class CurseOfAttrition extends Enchantment {
+    public CurseOfAttrition(Rarity weight, EquipmentSlot[] slotTypes) {
+        super(weight, EnchantmentTarget.WEAPON, slotTypes);
+    }
+
+    @Override
+    public int getMinPower(int level) {
+        return 25;
+    }
+
+    @Override
+    public int getMaxPower(int level) {
+        return 50;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return true;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return true;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    public boolean isAcceptableItem(ItemStack stack) {
+        return stack.getItem() instanceof PickaxeItem || stack.getItem() instanceof AxeItem || stack.getItem() instanceof ShovelItem || stack.getItem() instanceof HoeItem || super.isAcceptableItem(stack);
+    }
+
+    @Override
+    protected boolean canAccept(Enchantment other) {
+        return !(other instanceof Swiftness) ;
+    }
+}

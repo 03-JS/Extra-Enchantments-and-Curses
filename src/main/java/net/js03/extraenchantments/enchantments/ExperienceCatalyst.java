8f1465a12cc49381365605501daf54a6c7089326
03-JS <pyjoso3vitaslim@gmail.com>
2023-11-03 14:23:56 +0100

    Create ExperienceCatalyst

---
+++ src/main/java/net/js03/extraenchantments/enchantments/ExperienceCatalyst.java
@@ -0,0 +1,29 @@
+package net.js03.extraenchantments.enchantments;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+public class ExperienceCatalyst extends Enchantment {
+    public ExperienceCatalyst(Rarity weight, EquipmentSlot[] slotTypes) {
+        super(weight, EnchantmentCategory.FISHING_ROD, slotTypes);
+    }
+
+    public int getMinCost(int level) {
+        return 1 + 10 * (level - 1);
+    }
+
+    public int getMaxCost(int level) {
+        return super.getMinCost(level) + 50;
+    }
+
+    public int getMaxLevel() {
+        return 5;
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack pStack) {
+        return pStack.getItem() instanceof SwordItem || pStack.getItem() instanceof PickaxeItem || pStack.getItem() instanceof AxeItem || pStack.getItem() instanceof ShovelItem || pStack.getItem() instanceof HoeItem || pStack.getItem() instanceof TridentItem || super.canEnchant(pStack);
+    }
+}

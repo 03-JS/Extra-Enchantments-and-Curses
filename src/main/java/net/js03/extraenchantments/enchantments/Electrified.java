e8679f8003b1d82b029c2856b450d8907c83be69
JS03 <pyjoso3vitaslim@gmail.com>
2024-01-09 20:36:48 +0100

    Update Electrified

--- src/main/java/net/js03/extraenchantments/enchantments/Electrified.java
+++ src/main/java/net/js03/extraenchantments/enchantments/Electrified.java
@@ -7,6 +7,7 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.DamageTypeTags;
 
@@ -15,11 +16,13 @@ public class Electrified extends Enchantment {
 
     private int hitsTarget;
     private int hitsAttacker;
+    private int rng;
 
     public Electrified(Rarity weight, EquipmentSlot[] slotTypes) {
         super(weight, ExtraEnchantsMain.CONFIG.electrified.target(), slotTypes);
         hitsTarget = 0;
         hitsAttacker = 0;
+        rng = 0;
     }
 
     @Override
@@ -69,39 +72,51 @@ public class Electrified extends Enchantment {
 
     @Override
     public void onTargetDamaged(LivingEntity user, Entity target, int level) {
-        int rng = (int) (Math.random() * 11);
-        if (target instanceof LivingEntity && ((LivingEntity) target).getRecentDamageSource() != null) {
-            if (user.getWorld().isThundering()) {
-                hitsTarget++;
-                if (rng <= 2 && hitsTarget == 1) {
-                    LightningEntity lightningEntity = EntityType.LIGHTNING_BOLT.create(target.getWorld());
-                    assert lightningEntity != null;
-                    lightningEntity.refreshPositionAfterTeleport(target.getX(), target.getY(), target.getZ());
-                    target.getWorld().spawnEntity(lightningEntity);
+        rng = (int) (Math.random() * 11);
+        if (user.getWorld().isThundering()) {
+            hitsTarget++;
+            if (target instanceof LivingEntity && ((LivingEntity) target).getRecentDamageSource() != null) {
+                if (target instanceof PlayerEntity) {
+                    summonLightning(target, hitsTarget);
+                } else {
+                    if (((LivingEntity) target).getHealth() > 5) {
+                        summonLightning(target, hitsTarget);
+                    }
                 }
-                if (hitsTarget == 4) {
-                    hitsTarget = 0;
-                }
+            }
+            if (hitsTarget == 4) {
+                hitsTarget = 0;
             }
         }
     }
 
     @Override
     public void onUserDamaged(LivingEntity user, Entity attacker, int level) {
-        int rng = (int) (Math.random() * 11);
-        if (attacker instanceof LivingEntity) {
-            if (user.getWorld().isThundering()) {
-                hitsAttacker++;
-                if (rng <= 2 && hitsAttacker == 1) {
-                    LightningEntity lightningEntity = EntityType.LIGHTNING_BOLT.create(attacker.getWorld());
-                    assert lightningEntity != null;
-                    lightningEntity.refreshPositionAfterTeleport(attacker.getX(), attacker.getY(), attacker.getZ());
-                    attacker.getWorld().spawnEntity(lightningEntity);
-                }
-                if (hitsAttacker == 4) {
-                    hitsAttacker = 0;
+        rng = (int) (Math.random() * 11);
+        if (user.getWorld().isThundering()) {
+            hitsAttacker++;
+            if (attacker instanceof LivingEntity) {
+                if (attacker instanceof PlayerEntity) {
+                    summonLightning(attacker, hitsAttacker);
+                } else {
+                    if (((LivingEntity) attacker).getHealth() > 5) {
+                        summonLightning(attacker, hitsAttacker);
+                    }
                 }
             }
+            if (hitsAttacker == 4) {
+                hitsAttacker = 0;
+            }
+        }
+    }
+
+    private void summonLightning(Entity entity, int hits) {
+        if (rng <= 2 && hits == 1) {
+            System.out.println("RNG: " + rng + "\nAttacker Hits: " + hitsAttacker + "\nTarget Hits: " + hitsTarget);
+            LightningEntity lightningEntity = EntityType.LIGHTNING_BOLT.create(entity.getWorld());
+            assert lightningEntity != null;
+            lightningEntity.refreshPositionAfterTeleport(entity.getX(), entity.getY(), entity.getZ());
+            entity.getWorld().spawnEntity(lightningEntity);
         }
     }
 }
